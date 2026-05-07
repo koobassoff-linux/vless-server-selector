30119e84a1fea8a6284569883fb13468351bbe32
Chucker <koobassoff@gmail.com>
2026-05-07 15:08:04 +0300

    Add files via upload
    * chmod *.sh, error handling;

--- asn-get.sh
+++ asn-get.sh
@@ -8,8 +8,13 @@ while [ "$#" -gt 0 ]; do
 	URL="${1}"
 
 	IP=$(resolveip -s "${URL}")
-	DESCR=$(curl -sL "ip.guide/${IP}")
 
+	DESCR=$(curl -sL "ip.guide/${IP}")
+	if [[ $? -gt 0 ]]; then
+		echo "ip.guide error"
+		shift
+		continue
+	fi
 	jq -n --argjson data "${DESCR}" '$data.network.autonomous_system.asn'
 
 	shift
